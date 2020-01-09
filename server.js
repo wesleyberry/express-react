d6739ddd37430b66cfbec70e4fbf86a6399f8d05
Wesley Berry <wesleyberry52@gmail.com>
2020-01-09 13:24:18 -0600

    mapped customers to screen

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 const express = require('express');
 const app = express();
 
+app.get('/api/customers', (req, res) => {
+    const customers = [
+        {
+            id: 1,
+            firstName: 'John',
+            lastName: 'Doe'
+        },
+        {
+            id: 2,
+            firstName: 'Steve',
+            lastName: 'Smith'
+        },
+        {
+            id: 3,
+            firstName: 'Mary',
+            lastName: 'Swanson'
+        },
+    ];
+
+    res.json(customers);
+});
+
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
